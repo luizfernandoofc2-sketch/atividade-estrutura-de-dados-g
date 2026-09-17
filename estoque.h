a564c025ddc0240f510d438d45dfe3e32667337d
Milleny Patrícia Menezes Oliveira <milleny.patricia@gmail.com>
2026-09-17 16:59:40 -0400

    Implementa pagamento a vista com desconto

--- estoque.h
+++ estoque.h
@@ -4,6 +4,7 @@
 #define MAX_ITENS 50
 #define ESTOQUE_MINIMO 5
 #define TAXA_PADRAO 0.10
+#define TAXA_DESCONTO 0.05
 
 typedef struct {
     int id;
@@ -17,5 +18,6 @@ typedef struct {
 void exibir_menu(void);
 void listar_produtos(Produto lista[], int total);
 float calcular_total(Produto lista[], int total);
+float aplicar_desconto(float total);
 
 #endif
